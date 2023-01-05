6cfe4a7847939b37f3b90dd9183f0ae374bf1045
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-05 13:34:26 -0800

    test vm template creation

---
+++ manifests/vm.pp
@@ -0,0 +1,27 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   libvirt::vm { 'namevar': }
+define libvirt::vm (
+  Integer       $cpus = 1,
+  Integer       $memory_mb = 1000,
+  Integer       $disk_gb = 10,
+  Array[Tuple[String, String, Integer]] $networks = []
+) {
+
+  # Create the domain definition
+  file { "creating ${name} domain definition":
+    ensure  => file,
+    path    => "/tmp/${name}_domain.xml",
+    content => epp('libvirt/vm.epp', {
+      'name'      => $name,
+      'uuid'      => 'FIGURE_OUT_HOW_TO_MAKE_A_UUID_IN_PUPPET',
+      'cpus'      => $cpus,
+      'memory_mb' => $memory_mb,
+      'disk_name' => 'PUT_REAL_DISK_NAME_HERE_AFTER_IT_IS_CREATED.qcow2',
+      'networks'  => $networks,
+      }),
+  }
+}
