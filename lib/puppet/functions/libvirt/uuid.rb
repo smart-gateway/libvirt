8c1a122d77d9b05e75b6a412c991bdde5ec828a3
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-05 17:06:39 -0800

    modify template

--- lib/puppet/functions/libvirt/uuid.rb
+++ lib/puppet/functions/libvirt/uuid.rb
@@ -1,6 +1,8 @@
+require 'securerandom'
+
 Puppet::Functions.create_function(:'libvirt::uuid') do
   def uuid()
     # Return a uuid
-    return 'this_is_not_a_real_uuid'
+    return SecureRandom.uuid
   end
 end
